9787796b822e27240af2ec7ff8ba6bd6bb164dfb
AngeliciaF <afrank55@students.kennesaw.edu>
2022-04-08 08:27:24 -0400

    Added ML confidences to the ML inference test file

--- raspberry_pi_backup/programs_that_were_running_on_the_pi/inference_ml_model_video.py
+++ raspberry_pi_backup/programs_that_were_running_on_the_pi/inference_ml_model_video.py
@@ -10,10 +10,15 @@ import seaborn as sn
 import time
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+# from segmentation2_kinect import reference_rotation, getPayloads, getBoundingBox, adjust_sample_center
+# from shapes_contour_test import plot_poly
+
 # from inference_ml_model_video import drawBoundingBox
 
 # Load the ML model
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='ml_files/best.pt')  # local model
+DPI = 72
 
 # Custom inference settings
 model.conf = 0.40           # NMS confidence threshold - Confidence value that the bounding box contains an object (Max = 1)
@@ -38,14 +43,17 @@ color = (255, 0, 0) # BGR
 thickness = 2
 
 # Choose camera
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
 while True:
     # Set start time for FPS calculations
     start_time = time.time()
 
     # Read from camera
-    ret, frame = cap.read()
+    # ret, frame = cap.read()
+    frame = cv2.imread("mtre4800-kawasaki-project/three_containers4.jpg")
+    frame = cv2.resize(frame[300:3500,300:3500],(640, 480))
+
         
     if frame is None:
         break
@@ -67,6 +75,8 @@ while True:
     print("Predicted Labels:\n", predicted_labels)
     print("Bounding Boxes Coordinates:\n", cord_thres)
 
+    # fig, ax = plt.subplots(figsize=(8.33333333, 8.33333333), dpi=DPI)
+
     # TODO: Use reference angle
     # Change bbox orientation/angle
     # Figure out scada tags stuff
@@ -74,21 +84,36 @@ while True:
         # Convert predicted class labels (numeric) to readable labels
         for i in range(len(predicted_labels)):
             row = cord_thres[i]
+            confidence = cord_thres[i][4]
             # If the confidence is > 0.6, predict_labels
             if row[4] >= 0.6:
                 if predicted_labels[i] == 0:
                     color = (255, 0, 0)
+                    label = "Black Box"
                     print("Black, plastic box was found!\n")
                 elif predicted_labels[i] == 1:
                     color = (0, 255, 0)
+                    label = "Orange Bucket"
                     print("Orange, plastic bucket was found!\n")
                 elif predicted_labels[i] == 2:
                     color = (0, 0, 255)
+                    label = "White Box"
                     print("White, Styrofoam box was found!\n")
                 else:
                     print("An error occurred!\n")
-                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                # TODO: Make the bounding box rotate to adjust to the container
+                x1, x2, y1, y2 = int(row[0]*x_shape), int(row[2]*x_shape), int(row[1]*y_shape), int(row[3]*y_shape)
+                center = (int((x1+x2)/2), int((y1+y2)/2))
+                cv2.circle(frame,center,3,color,-1)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                cv2.putText(frame, "{}: {} [{:.2f}]".format(predicted_labels[i], label, float(confidence)), 
+                    (x1,y1-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+            
+            # getBoundingBox(center, payload, frame, hsize=180)
+
+
+        
     else:
         print("No containers were found!\n")
 
@@ -121,7 +146,67 @@ def drawBoundingBox(color, frame, predicted_labels, cord_thres):
     for i in range(len(predicted_labels)):
         row = cord_thres[i]
         if row[4] >= 0.2:
-            x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+            x1, x2, y1, y2 = int(row[0]*x_shape), int(row[2]*x_shape), int(row[1]*y_shape), int(row[3]*y_shape)
             # color = (255, 0, 0)
             cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
             # cv2.putText(frame, predicted_labels[i], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+
+def getBoundingBox(center, frame, hsize=180): #payload
+    # center = (payload.x, payload.y)
+    # FIXME: Ask Tim: Can you explain this?
+    # center = adjust_sample_center(center, image.shape, hsize)
+    pt1 = (center[0]-hsize, center[1]-hsize)
+    pt2 = (center[0]+hsize, center[1]+hsize)
+    sample = frame[pt1[1]:pt2[1],pt1[0]:pt2[0]]
+    sample_template = frame[pt1[1]:pt2[1],pt1[0]:pt2[0]]
+
+    [tR, tG, tB] = cv2.split(sample_template)
+    [iR, iG, iB] = cv2.split(sample)
+
+    dR = cv2.absdiff(iR, tR)
+    dG = cv2.absdiff(iG, tG)
+    dB = cv2.absdiff(iB, tB)
+    bl = 19
+    cn = 15
+    tR = cv2.adaptiveThreshold(dR,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,bl,cn)
+    tG = cv2.adaptiveThreshold(dG,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,bl,cn)
+    tB = cv2.adaptiveThreshold(dB,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,bl,cn)
+    # TODO: experiment with merging channels
+    difference = cv2.merge([tR,tG,tB])
+    #cv2.imshow('fuzzy', cv2.merge([dR,dG,dB]))
+    difference = cv2.cvtColor(difference,cv2.COLOR_BGR2GRAY)
+    
+    k_size = 15
+    kernelmatrix = np.ones((k_size, k_size), np.uint8)
+    # TODO: Fix black boxes dilate add more white pixel around other white pixels based on k_size
+    d = cv2.dilate(difference, kernelmatrix)
+    
+    fuzzy = cv2.GaussianBlur(d, (9,9), 4)
+    
+    contours, _ = cv2.findContours(fuzzy, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    targets = []
+    for contour in contours:
+        bounds = cv2.minAreaRect(contour)
+        # if circle
+            # bounds[2]
+        # if check_size_hi(bounds):
+        targets.append(bounds)
+    
+    if len(targets) > 0:
+        # if len(targets) > 1:
+        #     target = checkOverlap(targets)
+        # else:
+        #     target = targets[0]
+    
+        adjusted_center = (target[0][0] + pt1[0], target[0][1] + pt1[1])
+        payload.x = int(adjusted_center[0])
+        payload.y = int(adjusted_center[1])
+        payload.r = reference_rotation(target)
+        target = (adjusted_center,target[1],target[2])
+        box = cv2.boxPoints(target)
+        box = np.int0(box)
+        return box, sample
+    else:
+        return None, sample
+
