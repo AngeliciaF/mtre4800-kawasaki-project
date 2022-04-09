e7e5cf3b877b546018797865fbfbe5c12741aa57
AngeliciaF <afrank55@students.kennesaw.edu>
2022-04-09 00:44:22 -0400

    Added adjustable windows for the RGB and depth video feeds

--- run_kinect_camera.py
+++ run_kinect_camera.py
@@ -1,4 +1,5 @@
 # Run the Kinect camera
+from cv2 import WINDOW_NORMAL
 import freenect
 import cv2
 from matplotlib.colors import rgb2hex
@@ -6,21 +7,27 @@ import numpy as np
 
 def main():
     print("Starting the Kinect Camera")
+    rgb_window_name = "RGB Video Feed"
+    depth_window_name = "Depth Video Feed"
+    cv2.namedWindow(rgb_window_name, WINDOW_NORMAL)
+    cv2.namedWindow(depth_window_name, WINDOW_NORMAL)
+
     while True:
         # Get the RGB image from the Kinect
         rgb_image, _ = freenect.sync_get_video()
         rgb_image = rgb_image.astype(np.uint8)
-        print(rgb_image.shape)
-        print(rgb_image.shape[0])
-        print(rgb_image.shape[1])
+        bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+        print(bgr_image.shape)
+        print(bgr_image.shape[0])
+        print(bgr_image.shape[1])
         # print(rgb_image)
         # Get the depth image from the Kinect
         depth_image, _ = freenect.sync_get_depth()
         depth_image = depth_image.astype(np.uint8)
 
         # Display RGB and Depth Image
-        cv2.imshow("RGB Image", rgb_image)
-        cv2.imshow("Depth Image", depth_image)
+        cv2.imshow(rgb_window_name, bgr_image)
+        cv2.imshow(depth_window_name, depth_image)
 
         # End live video feed with 'q' or Esc
         if cv2.waitKey(1) == ord('q') or cv2.waitKey(1) == 27:
